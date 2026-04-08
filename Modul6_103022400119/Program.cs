9131216f7f23bc1b19dfbb2c05b46b63fb2a6293
unknown <Daniarthawiku>
2026-04-08 07:33:42 +0700

    menambahkan dua class SayaMusicUser dan

--- Modul6_103022400119/Program.cs
+++ Modul6_103022400119/Program.cs
@@ -1,1 +1,69 @@
-﻿
+﻿public class SayaMusicUser
+{
+    private int id;
+    public string username;
+    private List<SayaMusicTrack> uploadedTracks;
+
+    public SayaMusicUser (string username)
+    {
+        this.username = username;
+    }
+
+    public int GetTotalPlayCount
+    {
+        get
+        {
+            return uploadedTracks.Count;
+        }
+    }
+    
+    public void AddTrack(SayaMusicTrack track)
+    {
+        this.uploadedTracks.Add(track);
+    }
+    
+    public void PrintAllTrack()
+    {
+        Console.WriteLine($"User:{username}");
+        for (int i = 0; i < uploadedTracks.Count; i++)
+        {
+            Console.WriteLine($"Track" + uploadedTracks.Count + uploadedTracks[i].title);
+        }
+    }
+}
+
+public class SayaMusicTrack
+{
+    private int id = 5;
+    public string title;
+    private int playCount;
+
+    public SayaMusicTrack(string title)
+    {
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        this.playCount = count;
+    }
+
+    public void PrintTrackDetails()
+    {
+        Console.WriteLine($"Id: {id}");
+        Console.WriteLine($"Judul: {title}");
+        Console.WriteLine($"Play Count: {playCount}");
+    }
+}
+
+public class Program
+{
+    static void main (string[] args)
+    {
+        SayaMusicTrack track = new SayaMusicTrack("lagu sedih");
+        int playTest = Convert.ToInt32 (Console.ReadLine());
+        track.IncreasePlayCount(playTest);
+        track.PrintTrackDetails();
+    }
+}
